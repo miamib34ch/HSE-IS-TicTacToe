9e5d2d27ef70e445f93748ec9820062a3516ebb4
Bogdan <bogdan.polygalov@gmail.com>
2023-10-25 14:31:13 +0500

    Initial Commit

---
+++ HSE-AI-TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HSE-AI-TicTacToe
+//
+//  Created by Богдан Полыгалов on 25.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
